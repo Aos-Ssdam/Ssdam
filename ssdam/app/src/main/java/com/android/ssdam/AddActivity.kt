ba3ebd6522aa2f26d518889f4d732fe6f057df03
gimbolam <3017129@naver.com>
2021-03-12 00:41:11 +0900

    보람 => SelectColorActivity.kt border 추가 AddActivity.kt 작성누르면 메인으로 이동

--- ssdam/app/src/main/java/com/android/ssdam/AddActivity.kt
+++ ssdam/app/src/main/java/com/android/ssdam/AddActivity.kt
@@ -1,11 +1,21 @@
 package com.android.ssdam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class AddActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
+
+        // 작성버튼
+        var AddBtn = findViewById<TextView>(R.id.tv_DiaryAdd)
+        AddBtn.setOnClickListener {
+            // 내용이 null이 아니면 추가
+            startActivity(Intent(this,MainActivity::class.java)) }
     }
-}
+
+}//====
