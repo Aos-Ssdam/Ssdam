82abed8d266fe02bc752eee971b0a0517d3f3e9e
gimbolam <3017129@naver.com>
2021-03-12 01:44:58 +0900

    보람 => 테마변경, 메인에 버튼추가

--- ssdam/app/src/main/java/com/android/ssdam/MainActivity.kt
+++ ssdam/app/src/main/java/com/android/ssdam/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.android.ssdam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+        // 나중에 지울꺼... 지금은 이동이 필요해서정
+
+        // 설정
+        var moveSetting = findViewById<TextView>(R.id.btn_Movesetting)
+        moveSetting.setOnClickListener {
+            // 내용이 null이 아니면 추가
+            startActivity(Intent(this,SettingActivity::class.java))
+        }
+
+        // 색선택
+        var moveSelect = findViewById<TextView>(R.id.btn_MoveselectColor)
+        moveSelect.setOnClickListener {
+            // 내용이 null이 아니면 추가
+            startActivity(Intent(this,SelectColorActivity::class.java))
+        }
+
+        // 디테일
+        var moveDetail = findViewById<TextView>(R.id.btn_MoveDetail)
+        moveDetail.setOnClickListener {
+            // 내용이 null이 아니면 추가
+            startActivity(Intent(this,DetailActivity::class.java))
+        }
+
+    }//onCreate
+
 }
