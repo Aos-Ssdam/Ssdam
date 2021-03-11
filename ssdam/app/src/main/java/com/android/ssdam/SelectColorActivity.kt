d5fc81759eb1dc52632d81ae03e4bf43ad3ad8b3
gimbolam <3017129@naver.com>
2021-03-11 18:55:49 +0900

    보람 => SelectColorActivity.kt 관련 이미지추가 아이콘추가, Splash.kt 완료 선택시 border 하고 있었음

--- ssdam/app/src/main/java/com/android/ssdam/SelectColorActivity.kt
+++ ssdam/app/src/main/java/com/android/ssdam/SelectColorActivity.kt
@@ -1,11 +1,33 @@
 package com.android.ssdam
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ImageView
+
 
 class SelectColorActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_color)
+
+        val test = findViewById<ImageView>(R.id.iv_Yellow)
+       test.setOnClickListener(object : View.OnClickListener{
+           override fun onClick(v: View?) {
+               when(v?.id){
+                   
+               }
+           }
+       })
+
+
     }
 }
+
+
+
--- ssdam/app/src/main/java/com/android/ssdam/Splash.kt
+++ ssdam/app/src/main/java/com/android/ssdam/Splash.kt
@@ -1,11 +1,22 @@
 package com.android.ssdam
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class Splash : AppCompatActivity() {
+
+    val SPLASH_VIEW_TIME: Long = 2000 // 2초간 스플래시 화면 노출
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this,SelectColorActivity::class.java))
+            finish()
+        }, SPLASH_VIEW_TIME)
+
     }
 }
