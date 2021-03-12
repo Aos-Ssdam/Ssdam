728b837b1dcdf5507979b186da397dcb5d93486d
ParkSeMi <qkrtpa12@naver.com>
2021-03-12 12:24:46 +0900

    21.03.12 / 12:24 / 박세미 => 비밀번호 하는중

--- ssdam/app/src/main/java/com/android/ssdam/FindpwActivity.kt
+++ ssdam/app/src/main/java/com/android/ssdam/FindpwActivity.kt
@@ -2,10 +2,26 @@ package com.android.ssdam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckBox
+import android.widget.EditText
+import android.widget.Toast
+import androidx.core.view.isVisible
 
 class FindpwActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_findpw)
+
+        val cb_password = findViewById<CheckBox>(R.id.cb_password)
+        val et_password = findViewById<EditText>(R.id.et_password)
+
+        cb_password.setOnCheckedChangeListener { buttonView, isChecked ->
+            if(isChecked){
+                et_password.isVisible = true
+            }else{
+                et_password.isVisible = false
+            }
+        }
+
     }
 }
