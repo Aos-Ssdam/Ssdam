fea529b5982968e8be12dd221cbab35abfb687df
ParkSeMi <qkrtpa12@naver.com>
2021-03-17 22:54:45 +0900

    21.03.17 / 22:55 / 박세미 => 전화번호 인증 하는중

--- ssdam/app/src/main/java/com/android/ssdam/CheckEmailActivity.kt
+++ ssdam/app/src/main/java/com/android/ssdam/CheckEmailActivity.kt
@@ -1,12 +1,18 @@
-package com.android.ssdam
+    package com.android.ssdam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.core.view.isVisible
+import com.google.firebase.FirebaseException
+import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthProvider
+import java.util.concurrent.TimeUnit
 import java.util.regex.Matcher
 import java.util.regex.Pattern
 
@@ -15,54 +21,38 @@ class CheckEmailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_check_email)
 
+        val tv_pwcheckEmail = findViewById<TextView>(R.id.tv_pwcheckEmail)
         val et_CheckEmail = findViewById<EditText>(R.id.et_checkEmail)
+        val btn_sendEmail = findViewById<Button>(R.id.btn_sendEmail)
         val btn_check = findViewById<Button>(R.id.btn_check)
 
         var pref = getSharedPreferences("pref", 0)
         var saveEmail = pref.getString("email", "")
 
-        // 확인 버튼 눌렀을때
+        // 저장된 이메일로 발송 메세지
+        tv_pwcheckEmail.setText(saveEmail + "로 \n 인증번호를 발송합니다.")
+
+        // 발송하기 버튼 눌렀을때
+        btn_sendEmail.setOnClickListener {
+            // 전화번호 인증 발송
+            et_CheckEmail.isVisible = true
+            btn_check.isVisible = true
+
+
+        }
+
+        // 확인 버튼 눌렀을때 인증번호 맞는지 판단
         btn_check.setOnClickListener {
 
-            var checkEmail = et_CheckEmail.text.toString()
-            Log.d("태그", "이메일 확인 들어오니" + checkEmail)
-            if (!isEmail(checkEmail)){
+            // 이메일인증 구현
 
-                var builder = AlertDialog.Builder(this)
-                builder.setTitle("알림")
-                builder.setMessage("잘못된 이메일 형식입니다.")
-                builder.setIcon(R.mipmap.ic_launcher)
-                builder.setPositiveButton("확인", null)
-
-                builder.show()
-
-            }else{
-                if(checkEmail != saveEmail){
-                    var builder = AlertDialog.Builder(this)
-                    builder.setTitle("알림")
-                    builder.setMessage("등록된 이메일과 다릅니다.")
-                    builder.setIcon(R.mipmap.ic_launcher)
-                    builder.setPositiveButton("확인", null)
-
-                    builder.show()
-
-                    et_CheckEmail.setText("")
-                }else{
-                    //이메일 인증 구현
-                }
-            }
         }
+
     } //----------
 
-    // 이메일 형식 체크
-    open fun isEmail(email: String?): Boolean {
-        var returnValue = false
-        val regex = "^[_a-zA-Z0-9-\\.]+@[\\.a-zA-Z0-9-]+\\.[a-zA-Z]+$"
-        val p: Pattern = Pattern.compile(regex)
-        val m: Matcher = p.matcher(email)
-        if (m.matches()) {
-            returnValue = true
-        }
-        return returnValue
-    }
+
+
+
+
+
 }
