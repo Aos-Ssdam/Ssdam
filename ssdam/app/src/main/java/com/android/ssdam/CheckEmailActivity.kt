55ad22254058d3ae03cbeca3668c98c1d5b4837f
ParkSeMi <qkrtpa12@naver.com>
2021-03-20 11:15:04 +0900

    21.03.20 / 11:14 / 박세미 => 문자 인증 하는중

--- ssdam/app/src/main/java/com/android/ssdam/CheckEmailActivity.kt
+++ ssdam/app/src/main/java/com/android/ssdam/CheckEmailActivity.kt
@@ -11,7 +11,10 @@ import androidx.appcompat.app.AlertDialog
 import androidx.core.view.isVisible
 import com.google.firebase.FirebaseException
 import com.google.firebase.auth.PhoneAuthCredential
+import com.google.firebase.auth.PhoneAuthOptions
 import com.google.firebase.auth.PhoneAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import java.util.concurrent.TimeUnit
 import java.util.regex.Matcher
 import java.util.regex.Pattern
@@ -21,22 +24,53 @@ class CheckEmailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_check_email)
 
-        val tv_pwcheckEmail = findViewById<TextView>(R.id.tv_pwcheckEmail)
-        val et_CheckEmail = findViewById<EditText>(R.id.et_checkEmail)
-        val btn_sendEmail = findViewById<Button>(R.id.btn_sendEmail)
+        val tv_pwcheckPhone = findViewById<TextView>(R.id.tv_pwcheckPhone)
+        val et_CheckPhone = findViewById<EditText>(R.id.et_checkPhone)
+        val btn_sendPhone = findViewById<Button>(R.id.btn_sendPhone)
         val btn_check = findViewById<Button>(R.id.btn_check)
 
         var pref = getSharedPreferences("pref", 0)
-        var saveEmail = pref.getString("email", "")
+        var savePhone = pref.getString("phone", "")
 
-        // 저장된 이메일로 발송 메세지
-        tv_pwcheckEmail.setText(saveEmail + "로 \n 인증번호를 발송합니다.")
+        // 저장된 번호에 문자 발송 메세지
+        tv_pwcheckPhone.setText(savePhone + "로 \n인증번호를 발송합니다.")
 
         // 발송하기 버튼 눌렀을때
-        btn_sendEmail.setOnClickListener {
-            // 전화번호 인증 발송
-            et_CheckEmail.isVisible = true
+        btn_sendPhone.setOnClickListener {
+            // 문자발송 구현
+            et_CheckPhone.isVisible = true
             btn_check.isVisible = true
+
+            val auth = Firebase.auth
+            val options = PhoneAuthOptions.newBuilder(auth)
+                .setPhoneNumber(savePhone)  // 전화번호
+                .setTimeout(60L, TimeUnit.SECONDS)
+                .setActivity(this)
+                .setCallbacks(object : PhoneAuthProvider.OnVerificationStateChangedCallbacks(){
+
+                    override fun onCodeSent(
+                        verificationId: String,
+                        forceResendingToken: PhoneAuthProvider.ForceResendingToken
+                    ) {
+                        // Save the verification id somewhere
+                        // ...
+                        Log.d("verificationId", "인증번호 : " + verificationId)
+                        // The corresponding whitelisted code above should be used to complete sign-in.
+                        this@CheckEmailActivity.enableUserManuallyInputCode()
+                    }
+
+                    override fun onVerificationCompleted(phoneAuthCredential: PhoneAuthCredential) {
+                        // Sign in with the credential
+                        // ...
+                    }
+
+                    override fun onVerificationFailed(e: FirebaseException) {
+                        // ...
+                    }
+
+                })
+                .build()
+            PhoneAuthProvider.verifyPhoneNumber(options)
 
 
         }
@@ -44,7 +78,9 @@ class CheckEmailActivity : AppCompatActivity() {
         // 확인 버튼 눌렀을때 인증번호 맞는지 판단
         btn_check.setOnClickListener {
 
-            // 이메일인증 구현
+            // 문자발송 구현
+
+
 
         }
 
@@ -53,6 +89,8 @@ class CheckEmailActivity : AppCompatActivity() {
 
 
 
-
+    private fun enableUserManuallyInputCode() {
+        // No-op
+    }
 
 }
--- ssdam/app/src/main/java/com/android/ssdam/FindpwActivity.kt
+++ ssdam/app/src/main/java/com/android/ssdam/FindpwActivity.kt
@@ -20,8 +20,8 @@ class FindpwActivity : AppCompatActivity() {
 
         val cb_password = findViewById<CheckBox>(R.id.cb_password)
         val et_password = findViewById<EditText>(R.id.et_password)
-        val et_email = findViewById<EditText>(R.id.et_email)
-        val btn_pwOk = findViewById<Button>(R.id.btn_pwOk)
+        val et_phone = findViewById<EditText>(R.id.et_phone)
+        val btn_phoneOk = findViewById<Button>(R.id.btn_phoneOk)
 
 
         // 실행시 저장된 비밀번호가 있으면 체크박스 체크
@@ -31,12 +31,12 @@ class FindpwActivity : AppCompatActivity() {
         cb_password.setOnCheckedChangeListener { buttonView, isChecked ->
             if(cb_password.isChecked == true){
                 et_password.isEnabled = true
-                et_email.isEnabled = true
-                btn_pwOk.isEnabled = true
+                et_phone.isEnabled = true
+                btn_phoneOk.isEnabled = true
             }else if(cb_password.isChecked == false){
                 et_password.isEnabled = false
-                et_email.isEnabled = false
-                btn_pwOk.isEnabled = false
+                et_phone.isEnabled = false
+                btn_phoneOk.isEnabled = false
 
                 // 비밀번호 등록 되어 있던 SharedPreferences 초기화
                 var pref = getSharedPreferences("pref", 0)
@@ -48,10 +48,10 @@ class FindpwActivity : AppCompatActivity() {
         }
 
         // 저장 버튼 클릭, 입력된 비밀번호, 이메일 SharedPreferences에 저장
-        btn_pwOk.setOnClickListener {
+        btn_phoneOk.setOnClickListener {
 
             var setpw = et_password.text.toString()
-            var setemail = et_email.text.toString()
+            var setphone = et_phone.text.toString()
 
             if (setpw.length != 6){
                 var builder = AlertDialog.Builder(this)
@@ -64,10 +64,10 @@ class FindpwActivity : AppCompatActivity() {
 
             }else{
 
-                if(!isEmail(setemail)){
+                if(setphone.length != 11){
                     var builder = AlertDialog.Builder(this)
                     builder.setTitle("알림")
-                    builder.setMessage("잘못된 이메일 형식입니다.")
+                    builder.setMessage("전화번호를 정확하게 입력해주세요.")
                     builder.setIcon(R.mipmap.ic_launcher)
                     builder.setPositiveButton("확인", null)
 
@@ -76,11 +76,11 @@ class FindpwActivity : AppCompatActivity() {
                     var pref = getSharedPreferences("pref", 0)
                     var edit = pref.edit()  // 수정모드
                     edit.putString("pw", setpw)    // 1번째 인자에는 키 값을, 2번쨰 인자에는 실제 담아둘 값
-                    edit.putString("email", setemail)
+                    edit.putString("phone", setphone)
                     edit.apply()    // 값을 저장 완료
 
                     Toast.makeText(this, "정보가 저장되었습니다.", Toast.LENGTH_SHORT).show()
-                    Log.d("태그","설정 페이지 비밀번호 & 이메일 : " + pref.getString("pw","") + " & " + pref.getString("email", ""))
+                    Log.d("태그","설정 페이지 비밀번호 & 이메일 : " + pref.getString("pw","") + " & " + pref.getString("phone", ""))
 
                     val intent = Intent(this, SettingActivity::class.java)
                      startActivity(intent)
@@ -89,42 +89,30 @@ class FindpwActivity : AppCompatActivity() {
         }
     }
 
-    // 비밀번호, 이메일 저장 되어있는지 체크, 체크박스
+    // 비밀번호, 전화번호 저장 되어있는지 체크, 체크박스
     fun checkPw(){
         var pref = getSharedPreferences("pref", 0)
         var savePw = pref.getString("pw", "")
 
         val cb_password = findViewById<CheckBox>(R.id.cb_password)
         val et_password = findViewById<EditText>(R.id.et_password)
-        val et_email = findViewById<EditText>(R.id.et_email)
-        val btn_pwOk = findViewById<Button>(R.id.btn_pwOk)
+        val et_phone = findViewById<EditText>(R.id.et_phone)
+        val btn_pwOk = findViewById<Button>(R.id.btn_phoneOk)
 
         Log.d("태그", "저장된 비밀번호 : " +savePw)
 
         if(savePw == ""){
             cb_password.isChecked = false
             et_password.isEnabled = false
-            et_email.isEnabled = false
+            et_phone.isEnabled = false
             btn_pwOk.isEnabled = false
         }else{
             cb_password.isChecked = true
             et_password.isEnabled = true
-            et_email.isEnabled = true
+            et_phone.isEnabled = true
             btn_pwOk.isEnabled = true
         }
 
-    }
-
-    // 이메일 형식 체크
-    open fun isEmail(email: String?): Boolean {
-        var returnValue = false
-        val regex = "^[_a-zA-Z0-9-\\.]+@[\\.a-zA-Z0-9-]+\\.[a-zA-Z]+$"
-        val p: Pattern = Pattern.compile(regex)
-        val m: Matcher = p.matcher(email)
-        if (m.matches()) {
-            returnValue = true
-        }
-        return returnValue
     }
 
 }
